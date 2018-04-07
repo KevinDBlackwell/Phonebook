30c6942e0dd916913fb3b14b4cb86bb37a33f09c
kevindblackwell <kevinblackwell414@gmail.com>
2018-04-07 13:40:26 -0400

    added submit functionality to create.php

--- public_html/create.php
+++ public_html/create.php
@@ -1,4 +1,42 @@
+<?php
+
+    if (isset($_POST['submit'])) {
+        require "../resources/config.php";
+        require "../resources/common.php";
+
+        try {
+            $connection = new PDO($dsn, $username, $password, $options);
+            
+            $new_number = array(
+                "firstname" => $_POST['firstname'],
+                "lastname" => $_POST['lastname'],
+                "email" => $_POST['email'],
+                "phonenumber" => $_POST['phonenumber'],
+            );
+
+            $sql = sprintf(
+                "INSERT INTO %s (%s) values (%s)",
+                "people",
+                implode(", ", array_keys($new_number)),
+                ":" . implode(", :", array_keys($new_number))
+            );
+            
+            $statement = $connection->prepare($sql);
+            $statement->execute($new_number);
+        } 
+        
+        catch(PDOException $error) {
+            echo $sql . "<br>" . $error->getMessage();
+        }
+    }
+?>
+
+
 <?php include "../resources/templates/header.php" ?>
+
+<?php if (isset($_POST['submit']) && $statement) { ?>
+	<blockquote><?php echo $_POST['firstname']; ?> successfully added.</blockquote>
+<?php } ?>
 
 <h2>Add a Phone Number</h2>
 
