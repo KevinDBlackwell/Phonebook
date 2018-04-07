319e3958d9b3daad29b888f29733ac3254cdb1f6
kevindblackwell <kevinblackwell414@gmail.com>
2018-04-07 12:05:25 -0400

    created landing page

---
+++ public_html/index.php
@@ -0,0 +1,10 @@
+<?php include "../resources/templates/header.php" ?>
+
+	<h1>Phonebook</h1>
+
+	<ul>
+		<li><a href="create.php">Create</a> - Add a number</li>
+		<li><a href="view.php">View</a> - Find a number</li>
+	</ul>
+
+<?php include "../resources/templates/footer.php" ?>
