62cd7c6fbe230f8aa753c6f4b1c0d1c84093b242
kevindblackwell <kevinblackwell414@gmail.com>
2018-04-10 12:59:07 -0400

    Heroku Database Issue Fix

--- resources/config.php
+++ resources/config.php
@@ -1,9 +1,9 @@
 <?php
 
-    $host = "localhost";
+    $host = "i5x1cqhq5xbqtv00.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
     $username = "dja9aftxv8m3kab2";
     $password = "pope6xlo8p0il3wr";
-    $dbname = "phonebook";
+    $dbname = "if94rqzlurbk8y1p";
     $dsn = "mysql:host=$host; dbname=$dbname";
     $options = array(
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
