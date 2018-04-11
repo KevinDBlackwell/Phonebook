4462fbf3f999f0825313964fc97274ae43562c23
kevindblackwell <kevinblackwell414@gmail.com>
2018-04-11 11:34:59 -0400

    increased db access security

--- resources/config.php
+++ resources/config.php
@@ -1,9 +1,13 @@
 <?php
 
-    $host = "i5x1cqhq5xbqtv00.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-    $username = "dja9aftxv8m3kab2";
-    $password = "pope6xlo8p0il3wr";
-    $dbname = "if94rqzlurbk8y1p";
+    $url = getenv('JAWSDB_URL');
+    $dbparts = parse_url($url);
+
+    $host = $dbparts['host'];
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
+    $dbname = ltrim($dbparts['path'],'/');
+
     $dsn = "mysql:host=$host; dbname=$dbname";
     $options = array(
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
